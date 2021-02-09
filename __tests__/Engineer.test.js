e161d868a8a264c9aaa6b65227a7652abd8c5b9c
dvicjon <devinvjones@gmail.com>
2021-02-09 15:13:32 -0500

    Engineer object passing new tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,23 @@
-const Employee = require('../lib/Employee');
-jest.mock('../lib/Employee.js');
+const Engineer = require('../lib/Engineer')
+// test('creates an engineer object', () => {
+//     const employee = new Employee ('Devin, 4455,dldkldk,eepe,dvicj');
 
-test('creates an engineer object', () => {
-    const employee = new Employee ('Devin, 4455,dldkldk,eepe,dvicj');
+//     expect(employee.name).toEqual(expect.any(String));
+//     expect(employee.id).toEqual(expect.any(Number));
+//     expect(employee.email).toEqual(expect.any(String));
+//     expect(employee.role).toEqual(expect.any(String));
+//     expect(employee.github).toEqual(expect.any(String));
+// })
 
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
-    expect(employee.role).toEqual(expect.any(String));
-    expect(employee.github).toEqual(expect.any(String));
-})
+test('getRole() = Engineer', () => {
+    const role = "Engineer";
+    const x = new Engineer("Devin", 45, "email@github.com", "dvicj");
+    expect(x.getRole()).toBe(role);
+}); 
+
+test('getGitHub()', () => {
+    const github = "dvicj";
+    const x = new Engineer("Devin", 65, "gmail@gmail.com", github); 
+    expect(x.getGitHub()).toBe(github);
+
+});
