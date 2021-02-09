8616eca7162d8aa16918f0df5b21c98ddec20e29
dvicjon <devinvjones@gmail.com>
2021-02-09 11:19:34 -0500

    edits

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Team-Profile-Generator
+
+https://stackoverflow.com/questions/44961352/inquirer-js-input-answer-is-exiting-process-when-hitting-enter
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,7 +4,7 @@ const Employee = require('../lib/Employee');
 test('creates an employee object', () => {
     const employee = new Employee ('Devin Jones', 6575, 'skjskj', 'skjs');
 
-    expect(employee.name).toBe('Devin Jones');
+    expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
     expect(employee.role).toEqual(expect.any(String));
