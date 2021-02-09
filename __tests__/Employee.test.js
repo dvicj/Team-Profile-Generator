91d5da812772203a6769283487e97cbe62362b8c
dvicjon <devinvjones@gmail.com>
2021-02-09 13:18:38 -0500

    updating Employee object tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,5 @@
 jest.mock('../lib/Employee.js');
+const { getMaxListeners } = require('process');
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
@@ -8,4 +9,28 @@ test('creates an employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
     expect(employee.role).toEqual(expect.any(String));
-})
+});
+
+test('getName()', () => {
+    const name = "Devin";
+    const x = new Employee(name);
+    expect(x.getName().toBe(name));
+});
+
+test('getId()', () => {
+    const id = 24; 
+    const x = new Employee("Devin", id);
+    expect(x.getId().toBe(id));
+});
+
+test('getEmail()', () => {
+    const email = "email@gmail.com";
+    const x = new Employee("Devin", 24, email);
+    expect(x.getEmail().toBe(email)); 
+});
+
+test("getRole() = Employee", () => {
+    const role = "Employee";
+    const x = new Employee("Devin", 24, "email@gmail.com");
+    expect(x.getRole()).toBe(role);
+});
